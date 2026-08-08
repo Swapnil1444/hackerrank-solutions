24132f7a67e5e38e215451c4c70b1aa4444eb14f
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-08-08 16:55:30 +0530

    ✅ Java 1D Array [java] — approach1

---
+++ solutions/java/easy/java-1d-array-introduction/solution-approach1.java
@@ -0,0 +1,16 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=true
+// Problem     Java 1D Array
+// Difficulty  Easy
+// Subdomain   Data Structures
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-08, 04:55 p.m.
+// ──────────────────────────────────────────────────
+
+
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
