936737c21a64335d3102476e0a210be2213f54a5
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-08-13 19:47:48 +0530

    ✅ Java Strings Introduction [java] — approach1

---
+++ solutions/java/easy/java-strings-introduction/solution-approach1.java
@@ -0,0 +1,38 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-strings-introduction/problem?isFullScreen=true
+// Problem     Java Strings Introduction
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-13, 07:47 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int sum=(A.length()+B.length());
+        System.out.println(sum);
+        if (A.compareTo(B) > 0) {
+        System.out.println("Yes");
+         }else{
+             System.out.println("No");
+         }
+        String capA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capA + " " + capB);
+        
+    }
+}
+
+
+
