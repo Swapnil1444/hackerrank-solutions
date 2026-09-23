42833a86edb1a91208368ce91087e64601a7e563
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-09-23 23:50:14 +0530

    ✅ Java Inheritance II [java] — approach1

---
+++ solutions/java/easy/java-inheritance-2/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-inheritance-2/problem?isFullScreen=true
+// Problem     Java Inheritance II
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-23, 11:50 p.m.
+// ──────────────────────────────────────────────────
+
+
+
+//Write your code here
+class Arithmetic {
+    public int add(int n1 ,int n2){
+        return (n1+n2);
+    }
+}
+class Adder extends  Arithmetic{
+    
+}
+
