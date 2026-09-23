9b6902e23c49b63fbd3c1309f8d520598578b8fd
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-09-23 23:56:31 +0530

    ✅ Java Abstract Class [java] — approach1

---
+++ solutions/java/easy/java-abstract-class/solution-approach1.java
@@ -0,0 +1,21 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-abstract-class/problem?isFullScreen=true
+// Problem     Java Abstract Class
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-23, 11:56 p.m.
+// ──────────────────────────────────────────────────
+
+
+
+class MyBook extends Book{
+    void setTitle( String s){
+        super.title=s;
+    }
+    
+}
+//Write MyBook class here
+
