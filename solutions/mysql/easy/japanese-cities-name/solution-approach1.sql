81ce21b9f78623df6e89e251d3a2d677fcba8177
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-09-24 13:24:39 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-24, 01:24 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT name FROM city where COUNTRYCODE ="JPN";
