ecfca3a10ff2c511d44bda2aceb2c31d3409d8b1
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-09-24 11:59:22 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-24, 11:59 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * from CITY where ID=1661;
