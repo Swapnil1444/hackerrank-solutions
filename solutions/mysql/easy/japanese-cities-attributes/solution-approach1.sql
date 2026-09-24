d8b2f2e3b2505d07050dbee032929b1db53b51f7
Swapnil Supekar <swapnilsupekar9309@gmail.com>
2026-09-24 13:21:56 +0530

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-24, 01:21 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * from CITY where COUNTRYCODE ="JPN";
